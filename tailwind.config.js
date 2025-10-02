ba1a90453b29f451566db7c8a1b88581f0f19319
Dirgaaa <dirgayuditama6@gmail.com>
2025-10-02 22:18:16 +0700

    fix: fix daisy-ui and tailwind packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
-import daisyui from "daisyui";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -20,15 +19,4 @@ export default {
     },
 
     plugins: [forms, daisyui],
-
-    daisyui: {
-        themes: ["light", "dark", "corporate", "business"],
-        darkTheme: "business",
-        base: true,
-        styled: true,
-        utils: true,
-        prefix: "",
-        logs: true,
-        themeRoot: ":root",
-    },
 };
