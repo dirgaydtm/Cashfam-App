c24dcc817eac2f7a03f18272e3551fd28f6771f8
Dirgaaa <dirgayuditama6@gmail.com>
2025-10-02 22:19:32 +0700

    feat: create pattern animation for guest layout

--- resources/js/Components/authImagePatten.tsx
+++ resources/js/Components/authImagePatten.tsx
@@ -8,12 +8,11 @@ const AuthImagePattern = () => {
                     {[...Array(18)].map((_, i) => (
                         <div
                             key={i}
-                            className={`aspect-square rounded-2xl bg-primary ${i % 2 === 0 ? "animate-pulse" : ""
-                                }`}
+                            className={`aspect-square rounded-2xl  bg-primary ${i % 2 === 0 ? "animate-pulse" : ""} ${i % 2 !== 0 ? "animate-pulse [animation-delay:3s]" : ""}`}
                         />
                     ))}
                 </div>
-            
+
             </div>
         </div>
     );
